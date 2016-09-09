c2430739ae7f8bcea9710ec0c28ffe371c81b6b1
Douglas Henrique <douglash.sandro@gmail.com>
2016-09-09 13:13:31 -0300

    Control zoom position

--- src/directives/map.js
+++ src/directives/map.js
@@ -11,9 +11,9 @@
 			replace: true,
 			transclude: true,
 			scope: {
-				id: '@',
-				center: '=',
-				tileLayer: '='
+				tileLayer: '=',
+				optionsMap: '=',
+				controlOptions: '='
 			},
 			controller: function() {
 				var vm = this;
@@ -44,32 +44,45 @@
 			controllerAs: 'leafletMap',
 			template: '<div><div ng-transclude></div></div>',
 			link: function(scope, element, attrs, ctrl) {
-				var centerMap = L.latLng(dc.center.lat, dc.center.lng), minZoom = dc.minZoom, zoom = dc.zoom;
+				var centerMap = L.latLng(dc.center.lat, dc.center.lng), minZoom = dc.minZoom,
+				zoom = dc.zoom, zoomControl = dc.zoomControl;
 				var urlTemplate = dc.urlTemplate, options = dc.options;
+				var controlOptions = {position: dc.posControlZoom};
 
 				if(scope.tileLayer && scope.tileLayer.urlTemplate) {
 					urlTemplate = scope.tileLayer.urlTemplate;
 					options = scope.tileLayer.options;
 				}
 
-				if(scope.center && scope.center.lat && scope.center.lng)
-					centerMap = L.latLng(scope.center.lat, scope.center.lng);
+				var optionsMap = scope.optionsMap;
+				if(optionsMap && optionsMap.center && optionsMap.center.lat && optionsMap.center.lng)
+					centerMap = L.latLng(optionsMap.center.lat, optionsMap.center.lng);
 				
-				if(scope.center && scope.center.zoom)
-					zoom = scope.center.zoom;
+				if(optionsMap && optionsMap.zoomControl === true)
+					zoomControl = true;
 
-				if(scope.center && scope.center.minZoom)
-					minZoom = scope.center.minZoom
+				if(optionsMap && optionsMap.zoom)
+					zoom = optionsMap.zoom;
 
+				if(optionsMap && optionsMap.minZoom)
+					minZoom = optionsMap.minZoom;
+
+				if(scope.controlOptions && scope.controlOptions.position)
+					controlOptions.position = scope.controlOptions.position;
 
 				var map = new L.Map(element[0], {
 					center: centerMap,
+					zoomControl: zoomControl,
 					minZoom: minZoom,
 					zoom: zoom,
 					layers: [
 						L.tileLayer(urlTemplate, options)
 					]
 				});
+
+				//config control zoom
+				L.control.zoom(controlOptions).addTo(map);
+
 				ctrl.initMap(map);
 			}
 		};
--- src/services/constants/default-constants.js
+++ src/services/constants/default-constants.js
@@ -3,6 +3,8 @@ angular.module('leaflet-angular')
 	var constants = {};
 	constants.urlTemplate = '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	constants.options = {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'};
+    constants.zoomControl = false;
+    constants.posControlZoom = 'topleft';
     constants.minZoom = 2;
     constants.zoom = 3;
     constants.center = {lat: 0, lng:0};
