99b459ca31d01c1604e8073d963d0a310ddf1f47
Douglas Henrique <douglash.sandro@gmail.com>
2016-09-09 03:15:07 -0300

    Correção constants

--- src/services/constants/default-constants.js
+++ src/services/constants/default-constants.js
@@ -4,7 +4,7 @@ angular.module('leaflet-angular')
 	constants.urlTemplate = '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	constants.options = {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'};
     constants.minZoom = 2;
-    constants.zoom = 3
+    constants.zoom = 3;
     constants.center = {lat: 0, lng:0};
     return constants;
 }());
